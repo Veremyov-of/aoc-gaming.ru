541d4d0f92a4b456af177004a47fe09dd2fe0363
Denis <veremyov.of@gmail.com>
2021-11-30 22:47:03 +0300

    create and add project

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".swiper", {
+    autoplay: {
+        delay: 4000,
+    },
+    speed: 800,
+    loop: true,
+    initialSlide: 1,
+    effect: "slide",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+  });
